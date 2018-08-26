257fa4f3d416d4e050299a7233305f20cedcd109
Christian Lüdemann <christianlydemann@gmail.com>
2018-08-26 12:15:32 +0200

    added seedrandom

--- src/app/core/feature-toggle/feature-toggle.service.ts
+++ src/app/core/feature-toggle/feature-toggle.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import seedrandom from 'seedrandom';
 
 export type featureTypes = {
   'killer-feature': number | boolean;
@@ -9,6 +10,7 @@ export type featureTypes = {
 })
 export class FeatureToggleService {
   public features: featureTypes;
+  public userId: string = 'some-user-id';
 
   constructor() {}
 
@@ -16,18 +18,11 @@ export class FeatureToggleService {
     const featureValue = this.features[feautureName];
 
     if (Number.isInteger(featureValue as number)) {
+      seedrandom(this.userId);
       const isEnabled = Math.floor(Math.random() * 100) <= featureValue;
       return isEnabled;
     } else {
       return featureValue as boolean;
     }
   }
-
-  private hashCode = function(s) {
-    var h = 0,
-      l = s.length,
-      i = 0;
-    if (l > 0) while (i < l) h = ((h << 5) - h + s.charCodeAt(i++)) | 0;
-    return h;
-  };
 }
